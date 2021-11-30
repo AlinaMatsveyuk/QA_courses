dcc4896467c4a8b6b438c2b2b24595dc51c54559
AlinaMatsveyuk <88329952+AlinaMatsveyuk@users.noreply.github.com>
2021-11-30 13:34:44 +0300

    Update SQL_DDL_commands.sql

--- SQL_DDL_commands.sql
+++ SQL_DDL_commands.sql
@@ -205,14 +205,14 @@ CREATE TABLE roles_employee (
 );
 
 INSERT INTO roles_employee (employee_id, role_id)
-VALUES (1, 2),
+VALUES  (1, 2),
 	(2, 4),
 	(4, 6),
 	(6, 8),
 	(8, 10),
 	(10, 12),
 	(12, 14),
-	14, 16),
+	(14, 16),
 	(16, 18),
 	(18, 20),
 	(20, 1),
